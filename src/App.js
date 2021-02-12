2f5d33d9c0b1712090c98c1f16102b4fa7b32015
Shpakova Alena <shpakovadev@gmail.com>
2021-02-12 11:19:24 +0300

    add basket, shopping pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Basket from 'pages/Basket'
+import Shopping from 'pages/Shopping'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route path="/" component={Shopping} exact />
+        <Route path="/basket" component={Basket} />
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
