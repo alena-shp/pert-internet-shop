4d17d5d0afda41d64566bf1b39bc886b74463db0
Shpakova Alena <shpakovadev@gmail.com>
2021-02-14 18:55:01 +0300

    change path route

--- src/components/ShopHeader/index.js
+++ src/components/ShopHeader/index.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Icon from 'components/Icon'
 import basket from 'assets/icons/basket.svg'
 
 import styles from './styles.module.scss'
 
-const ShopHeader = ({ count = 0 }) => {
+const ShopHeader = () => {
+  const cartTotalItems = useSelector(state => state.cartItems.reduce((sum, item) => sum + item.count, 0))
+
   return (
     <div className={styles.shopHeader}>
       <Link to="/" className={styles.title}>
         Internet Shop
       </Link>
-      <Link to="/basket" className={styles.countWrap}>
+      <Link to="/cart" className={styles.countWrap}>
         <Icon img={basket} />
-        <div className={styles.countItems}>{count}</div>
+        <div className={styles.countItems}>{cartTotalItems}</div>
       </Link>
     </div>
   )
